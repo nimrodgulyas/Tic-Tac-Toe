5a29a5c1dc89566f4c61adfdaaa811003be8b146
BulyakiBence <bulyaki.bence.benedikt@diak.szbi-pg.hu>
2026-05-26 15:49:10 +0200

    menu resze

--- menu.py
+++ menu.py
@@ -1,20 +1,11 @@
 def get_menu_option():
-    human_vs_human = 0
-    ai_vs_ai = 0
-    human_vs_ai = 0
-    valasztas = int(input("Give me a number according to the followings: \n1. Human vs Human \n2. Random AI vs Random AI(LATER) \n3. Human vs Random AI(LATER) \n The number: "))
-    while valasztas == 1 or 2 or 3:
-        if valasztas == 1:
-            human_vs_human += 1
-            return human_vs_human
-            break
-        elif valasztas == 2:
-            ai_vs_ai += 2
-            return ai_vs_ai
-            break
-        elif valasztas == 3:
-            human_vs_ai += 3
-            return human_vs_ai
-            break
-        elif valasztas != 1 or 2 or 3:
-            return("You didn't give a correct number!")
+    while True:
+        valasztas = input("Give me a number according to the followings: \n1. Human vs Human \n2. Random AI vs Random AI (LATER) \n3. Human vs Random AI (LATER) \nThe number: ").strip()
+        if valasztas == '1':
+            return 1
+        elif valasztas == '2':
+            return 2
+        elif valasztas == '3':
+            return 3
+        else:
+            print("You didn't give a correct number! Please try again.\n")
